506a789599754759755c0e2ecfd14a16ba114afd
Alex Oliveira <bsi.alexoliveira@gmail.com>
2016-03-08 16:20:14 -0300

    Flexibiliza o FullController

--- src/Controllers/Rest/FullController.php
+++ src/Controllers/Rest/FullController.php
@@ -10,34 +10,34 @@ class FullController extends BaseController
     public function index()
     {
         try {
-            $result = $this->service->search();
+            $data = $this->service->search();
         } catch (Exception $e) {
             throw $e;
         }
 
-        return response()->json($result->toArray(), 200);
+        return response()->json(is_array($data) ? $data : $data->toArray(), 200);
     }
 
     public function show()
     {
         try {
-            $obj = $this->service->read();
+            $data = $this->service->read();
         } catch (Exception $e) {
             throw $e;
         }
 
-        return response()->json($obj->toArray(), 200);
+        return response()->json(is_array($data) ? $data : $data->toArray(), 200);
     }
 
     public function store()
     {
         try {
-            $obj = $this->service->create();
+            $data = $this->service->create();
         } catch (Exception $e) {
             throw $e;
         }
 
-        return response()->json($obj->toArray(), 201);
+        return response()->json(is_array($data) ? $data : $data->toArray(), 201);
     }
 
     public function update()
